8b7126782f3996e24277b52c0ede0cf4ef65d5e7
Rafael <rafaelf.machado2004@gmail.com>
2022-12-16 16:07:55 -0300

    Adicionando botoes e layout

--- interface_grafica.py
+++ interface_grafica.py
@@ -8,7 +8,7 @@ def tela_inicial():
         [sg.Text('Informe o CEP:', font='arial 12', pad=(0, 0))],
         [sg.Input(size=(20, 0), font='arial 12', pad=(0, 0), key='cep')]
     ]
-    
+
     coluna1 = [
         [sg.Text('LOGRADOURO:', font='arial 12')],
         [sg.Text('BAIRRO:', font='arial 12')],
@@ -17,7 +17,7 @@ def tela_inicial():
         [sg.Text('IBGE:', font='arial 12')],
         [sg.Text('DDD:', font='arial 12')],
     ]
-    
+
     coluna2 = [
         [sg.Input(font='arial 12 bold', key='logradouro', size=(35, 1))],
         [sg.Input(font='arial 12 bold', key='bairro', size=(30, 1))],
@@ -26,3 +26,20 @@ def tela_inicial():
         [sg.Input(font='arial 12 bold', key='ibge', size=(15, 1))],
         [sg.Input(font='arial 12 bold', key='ddd', size=(4, 1))],
     ]
+
+    botoes = [
+        [sg.Button('Consultar', font='arial 12', size=(10, 1), pad=((0, 15), 0)),
+         sg.Button('Sair', font='arial 12', size=(8, 1))]
+    ]
+
+    layout = [
+        [sg.Text('ConsultarCEP', font='arial 18 bold')],
+        [sg.Column(cep, justification='center',
+                   element_justification='center')],
+        [sg.Column(coluna1, pad=((0, 20), 0)),
+         sg.Column(coluna2(2))],
+        [sg.Column(botoes, justification='center')]
+    ]
+
+    telaprin = sg.Window('ConsultaCEP', element_padding=(
+        0, 10), layout=layout, size=(600, 500), finalize=True)
